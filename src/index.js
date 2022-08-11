873a36ca601332c995367aca980f479b82fb4d82
Daniil Ermolovich <106492274+Aurtekh@users.noreply.github.com>
2022-08-12 01:42:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-module.exports = function check(str, bracketsConfig) {
-    let arr1 = [];
-    let arr2 = [];
-    let x = 1;
-    bracketsConfig.forEach((item) => {
-      arr1 = String(str).split('').filter(char => char === item[0]);
-      arr2 = String(str).split('').filter(char => char === item[1]);
-      if (arr1.length !== arr2.length) {
-        return x = 0;
-      }
-  });
-    return x === 1;
-  }
+
